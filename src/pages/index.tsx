f4f60dbb588d0012edde15184537dbf407689a7c
Trevor Allred <trevorallred@gmail.com>
2023-03-01 11:29:14 -0800

    remove rotate, better zoom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,10 @@
 import { Building, Layout } from '@/components'
-import { clsx } from 'clsx'
 import Link from 'next/link'
-import { useState } from 'react'
 
 function IndexPage() {
-  const [rotate, setRotate] = useState(false)
   return (
     <Layout>
-      <div className={clsx(['w-full h-full overflow-scroll', rotate && 'rotate-90'])}>
-        <Building />
-      </div>
-      <button
-        onClick={() => setRotate(!rotate)}
-        className="absolute bottom-0 right-0 px-4 py-1 bg-slate-700 text-white rounded-tl-xl hover:bg-slate-800"
-      >
-        Rotate {rotate}
-      </button>
+      <Building />
       <Link
         href="/about"
         className="absolute bottom-0 left-0 px-4 py-1 bg-slate-700 text-white rounded-tr-xl hover:bg-slate-800"
